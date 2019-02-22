9552b54af25610a5aa8299b2682f55cd353108e9
anharfathoni <anharaf28@gmail.com>
2019-02-22 13:23:38 +0700

    promotor

--- controllers/promotorController.js
+++ controllers/promotorController.js
@@ -1,21 +1,54 @@
 const Promotor = require('../models/Promotor')
 
 module.exports = {
-  create: (req,res) => {
+  create: (req, res) => {
+    let { name, email, gender, dob } = req.body
+    let newPromotor = { name, email, gender, dob }
+
+    Promotor
+      .create(newPromotor)
+      .then(promotor => {
+        res.status(201).json({ promotor })
+      })
+      .catch(error => {
+        res.status(400).json({ message: error.message })
+      })
+  },
+
+  getAll: (req, res) => {
 
   },
 
-  getAll: (req,res)=> {
-
-  },
-
-  getOne: (req,res) => {
-    let { prmotorId } = req.params
-    
-  },
-
-  edit: (req,res) => {
+  getOne: (req, res) => {
     let { promotorId } = req.params
+    Promotor
+      .findById(promotorId)
+      .then(promotor => {
+        res.json({ promotor })
+      })
+      .catch(error => {
+        res.status(400).json({ message: error.message })
+      })
+  },
 
+  edit: (req, res) => {
+    let { promotorId } = req.params
+    let { name, email, gender, dob } = req.body
+    let newPromotor = { name, email, gender, dob }
+
+    for (let key in newPromotor) {
+      if (newPromotor[key] === undefined) {
+        delete newPromotor[key]
+      }
+    }
+
+    Promotor
+      .findByIdAndUpdate(promotorId, newPromotor, { new: true })
+      .then(promotor => {
+        res.status(201).json({ promotor })
+      })
+      .catch(error => {
+        res.status(400).json({ message: error.message })
+      })
   }
 }
--- routes/promotor.js
+++ routes/promotor.js
@@ -3,7 +3,7 @@ var router = express.Router();
 const promotorController = require('../controllers/promotorController')
 
 router
-      .get('/', promotorController.getAll)
+      // .get('/', promotorController.getAll)
       .get('/:promotorId', promotorController.getOne)
       .post('/', promotorController.create)
       .put('/:promotorId', promotorController.edit)
