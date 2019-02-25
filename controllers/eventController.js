67bb43744e6e32910db34f76f16237ffffbf44ca
patriagani <patria.gani@gmail.com>
2019-02-25 12:14:32 +0700

    add description

--- controllers/eventController.js
+++ controllers/eventController.js
@@ -5,8 +5,8 @@ const StaticticAfter = require('../models/StatisticAfter')
 
 module.exports = {
   create: (req,res) => {
-    let { name, place, date, price, timeStart, timeEnd, latitude, longitude } =  JSON.parse(req.body.data)
-    let body = { name, place, date, price, timeStart, timeEnd, latitude, longitude }
+    let { name, place, date, price, timeStart, timeEnd, latitude, longitude, description } =  JSON.parse(req.body.data)
+    let body = { name, place, date, price, timeStart, timeEnd, latitude, longitude, description }
 
     let obj = {
       ...body,
